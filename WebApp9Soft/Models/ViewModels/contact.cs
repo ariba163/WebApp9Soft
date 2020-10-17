449de606d354508085f4792f01d1fb71d4737277
ariba163 <aribajawed163@gmail.com>
2020-10-17 16:58:38 +0500

    loader issuebresolved

---
+++ WebApp9Soft/Models/ViewModels/contact.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp9Soft.Models.ViewModels
+{
+    public class contact
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = " Name *")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = " Email *")]
+        [Required]
+        public string Email { get; set; }
+
+        [Display(Name = " Subject *")]
+        [Required]
+        public string Subject { get; set; }
+
+        [Display(Name = " Contact No *")]
+        [Required]
+        public string ContactNo { get; set; }
+
+        [Display(Name = " Please describe what you need.")]
+        [Required]
+        public string Message { get; set; }
+        public string RequestNumber { get; set; }
+
+
+    }
+}
